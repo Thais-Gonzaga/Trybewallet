7c7aabb8674208025d5e93ab98ca0b6138329b41
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-01 20:10:45 -0300

    feat: criando page Login, verificando senha e e-mail e redirecionando
     ref: #requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,75 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.verificInput = this.verificInput.bind(this);
+    this.onSalve = this.onSalve.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState(
+      { [name]: value },
+      () => this.verificInput(),
+    );
+  }
+
+  onSalve() {
+    console.log('clicou');
+  }
+
+  verificInput() {
+    const { email, password } = this.state;
+    const min = 5;
+    const disabled = !(/\w+@\w+.com/.test(email) && password.length > min);
+    this.setState({ disabled });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled } = this.state;
+    return (
+      <div>
+        <h4>Login</h4>
+        <label htmlFor="email">
+          Email:
+          <input
+            id="email"
+            data-testid="email-input"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            id="password"
+            data-testid="password-input"
+            name="password"
+            value={ password }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <Link to="/carteira">
+          <button
+            type="button"
+            disabled={ disabled }
+            onClick={ this.onSalve }
+          >
+            Entrar
+          </button>
+        </Link>
+      </div>
+    );
   }
 }
 
