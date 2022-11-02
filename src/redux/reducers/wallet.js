4b0e3f60f5686c333757ee5d9a6a25b1b33da988
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 12:01:41 -0300

    feat: criando reducer para wallet com os estados iniciais total e moeda
     ref: #requisito 2

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,21 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET_CURRENCY } from '../actions/index';
+
+const INITIAL_STATE = {
+  total: 0,
+  currency: 'BRL',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_CURRENCY:
+    return {
+      total: action.state.total,
+      currency: action.state.currency,
+    };
+
+  default: return state;
+  }
+};
+
+export default user;
