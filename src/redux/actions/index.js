1734b41cf03740ea6c56ee8c9cd1e5e87ec3f435
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 10:47:17 -0300

    feat: criando a action para salvar o  email
     ref: #requisito1

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,8 @@
 // Coloque aqui suas actions
+export const USER_EMAIL = 'USER_EMAIL';
+// ação do botão entrar para salvar o email
+// preciso de um parametro email
+export const actionEmail = (state) => ({
+  type: 'USER_EMAIL',
+  state,
+});
