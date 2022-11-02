2232c6f581a923eca06648ebb037c93f03347010
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 10:49:00 -0300

    feat: criando o user reducer
     ref: #requisito 1

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { USER_EMAIL } from '../actions/index';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case USER_EMAIL:
+    return { email: action.state };
+
+  default: return state;
+  }
+};
+
+export default user;
