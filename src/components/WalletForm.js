0551843706860b350b1f973f82da4f9a748238be
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 16:07:09 -0300

    feat: criando o formulario no componente WalletForm sem requisição à API
     ref: #requisito 3

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,101 @@
 import React, { Component } from 'react';
 
+const optionsMethod = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'];
+const optionsTag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
+
 class WalletForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      values: 0,
+      description: '',
+      currency: '',
+      method: '',
+      tag: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState(
+      { [name]: value },
+    );
+  }
+
   render() {
+    const { values, description, currency, method, tag } = this.state;
+
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="values">
+          Valor
+          <input
+            id="values"
+            data-testid="value-input"
+            name="values"
+            value={ values }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="description">
+          Descrição
+          <input
+            id="description"
+            data-testid="description-input"
+            name="description"
+            value={ description }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <label htmlFor="currency">
+          Moeda:
+          <select
+            data-testid="currency-input"
+            id="currency"
+            name="currency"
+            value={ currency }
+            onChange={ this.handleChange }
+            options={ [''] }
+          >
+            opção
+          </select>
+        </label>
+
+        <label htmlFor="method">
+          Metodo de pagamento:
+          <select
+            data-testid="method-input"
+            id="method"
+            name="method"
+            value={ method }
+            onChange={ this.handleChange }
+            options={ [''] }
+          >
+            {
+              optionsMethod.map((option) => <option key={ option }>{option}</option>)
+            }
+          </select>
+        </label>
+
+        <label htmlFor="tag">
+          Categoria:
+          <select
+            data-testid="tag-input"
+            id="tag"
+            name="tag"
+            value={ tag }
+            onChange={ this.handleChange }
+            options={ [''] }
+          >
+            {
+              optionsTag.map((option) => <option key={ option }>{option}</option>)
+            }
+          </select>
+        </label>
+
+      </form>
     );
   }
 }
