2b9c874e61383edf9a4f7d140a9d263ae4ad9844
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 10:41:12 -0300

    feat: manipulando as rotas do Login
     ref: #requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+// import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        {/* <Route exact path="/carteira" component={ Wallet } /> */}
       </Switch>
     </div>
   );
