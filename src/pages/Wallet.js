4197e3f167081b46c6502b658ab95316098dae0e
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 16:01:07 -0300

    feat: renderizando componente WalletForm na page Wallet
     ref: #requisito 3

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <WalletForm />
+      </>
     );
   }
 }
