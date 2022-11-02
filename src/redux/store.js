370ec182b0d141822b353afd0b11d62d6e7d76d1
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 10:43:24 -0300

    feat: criando store e adicionando window para o teste
     ref: #requisito 1

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,10 @@
-// configure aqui sua store
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import roootReducer from './reducers/index';
+
+const store = createStore(roootReducer, composeWithDevTools());
+
+export default store;
+if (window.Cypress) {
+  window.store = store;
+}
