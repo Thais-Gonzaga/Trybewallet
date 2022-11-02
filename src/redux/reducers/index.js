ee1a07bacfa2813db74b38aa082917c2de991dfc
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-02 10:45:53 -0300

    feat: criando um reducer raiz que combine todos reducers
     ref: #requisito1

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,3 +3,13 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+// roootReducer - reducer raiz:
+import { combineReducers } from 'redux';
+import user from './user';
+
+const roootReducer = combineReducers({
+  user,
+});
+
+export default roootReducer;
