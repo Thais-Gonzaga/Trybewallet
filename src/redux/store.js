6ed42ae85a833b813627b2bbd0d3b4f99e9c96f9
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-03 12:10:10 -0300

    feat: implementando thunk no store para usar funções no actions
     ref: # requisito 3

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import roootReducer from './reducers/index';
 
-const store = createStore(roootReducer, composeWithDevTools());
+const store = createStore(roootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
 if (window.Cypress) {
