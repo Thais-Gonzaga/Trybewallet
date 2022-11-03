bb7c42da278ab3b3d5f621e0e47189d164b34efc
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-03 12:14:04 -0300

    feat: criando case para moedas e removendo com filter a moeda 'USDT'
     ref: #requisito3

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,9 +1,12 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import { WALLET_CURRENCY } from '../actions/index';
+import {
+  WALLETFORM_CURRENCY_SUCESS,
+  WALLET_CURRENCY } from '../actions/index';
 
 const INITIAL_STATE = {
   total: 0,
   currency: 'BRL',
+  currencies: [],
 };
 
 const user = (state = INITIAL_STATE, action) => {
@@ -12,6 +15,10 @@ const user = (state = INITIAL_STATE, action) => {
     return {
       total: action.state.total,
       currency: action.state.currency,
+    };
+  case WALLETFORM_CURRENCY_SUCESS:
+    return {
+      currencies: action.state.filter((currency) => currency !== 'USDT'),
     };
 
   default: return state;
