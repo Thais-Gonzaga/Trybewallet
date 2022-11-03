807952ae2a235c76068cb4f63d65aaaf8452421c
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-03 12:12:06 -0300

    feat: implementando actions e chamando a fetchAppi no dispatch
     ref: #requisito 3

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,9 @@
 // Coloque aqui suas actions
+import fetchApi from '../../services/FetchApi';
+
 export const USER_EMAIL = 'USER_EMAIL';
 export const WALLET_CURRENCY = 'WALLET_CURRENCY';
+export const WALLETFORM_CURRENCY_SUCESS = 'WALLETFORM_CURRENCY_SUCESS';
 
 export const actionEmail = (state) => ({
   type: USER_EMAIL,
@@ -11,3 +14,13 @@ export const actionCurrency = (state) => ({
   type: WALLET_CURRENCY,
   state,
 });
+
+export const actionCurrencySucess = (state) => ({
+  type: WALLETFORM_CURRENCY_SUCESS,
+  state,
+});
+
+export const fetchEconCurrency = () => async (dispatch) => {
+  const currency = await fetchApi();
+  dispatch(actionCurrencySucess(currency));
+};
