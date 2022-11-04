755f1994e2ea3ba418011c3ed7909616b9e7d67d
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-04 19:06:34 -0300

    feat: adicionando actions para o click de salvar os dados
     ref: #requisito 4

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,6 +4,7 @@ import fetchApi from '../../services/FetchApi';
 export const USER_EMAIL = 'USER_EMAIL';
 export const WALLET_CURRENCY = 'WALLET_CURRENCY';
 export const WALLETFORM_CURRENCY_SUCESS = 'WALLETFORM_CURRENCY_SUCESS';
+export const WALLETFORM_SAVE = 'WALLETFORM_SAVE';
 
 export const actionEmail = (state) => ({
   type: USER_EMAIL,
@@ -23,4 +24,14 @@ export const actionCurrencySucess = (state) => ({
 export const fetchEconCurrency = () => async (dispatch) => {
   const currency = await fetchApi();
   dispatch(actionCurrencySucess(currency));
+};
+
+export const actionForms = (state) => ({
+  type: WALLETFORM_SAVE,
+  state,
+});
+
+export const fetchData = (state) => async (dispatch) => {
+  const currency = await fetchApi();
+  dispatch(actionForms({ ...state, exchangeRates: currency }));
 };
