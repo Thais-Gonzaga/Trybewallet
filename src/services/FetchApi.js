77d0a7b5e2226ff2234d92f91356c5c306301703
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-04 18:49:06 -0300

    fix: mudando o retorno da FetchApi para conseguir usar nas duas thunk
     ref: #requisito 4

--- src/services/FetchApi.js
+++ src/services/FetchApi.js
@@ -2,7 +2,7 @@ const fetchApi = async () => {
   const url = 'https://economia.awesomeapi.com.br/json/all';
   const fetchCurry = await fetch(url);
   const data = await fetchCurry.json();
-  return Object.keys(data);
+  return data;
 };
 
 export default fetchApi;
