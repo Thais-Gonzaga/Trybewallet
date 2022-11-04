bf2adf6af9f95a82e2972c1144d093231513ee81
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-04 19:12:07 -0300

    feat: acessando o array com as informações salvas do estado global para somar as despesas e exbir o total
     ref: #requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import { string, number } from 'prop-types';
+import { string, arrayOf, number } from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
-    const { emailHeader, totalHeader, currencyHeader } = this.props;
+    const { emailHeader, currencyHeader, expensesHeader } = this.props;
+    // console.log(expensesHeader);
+    // expensesHeader.map(({ exchangeRates, currency, value }) => exchangeRates[currency].ask * value);
+    const total = expensesHeader.reduce((acc, curr) => +acc + +curr, 0);
     return (
       <div>
         <p data-testid="email-field">{emailHeader}</p>
         <p data-testid="total-field">
-          {' '}
-          {totalHeader}
-          {' '}
+          { !expensesHeader.length
+            ? parseFloat(0).toFixed(2)
+            : parseFloat(total).toFixed(2)}
         </p>
         <p data-testid="header-currency-field">{currencyHeader}</p>
       </div>
@@ -19,21 +22,25 @@ class Header extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  emailHeader: state.user.email,
-  currencyHeader: state.wallet.currency,
-  totalHeader: state.wallet.total,
-});
+const mapStateToProps = (state) => (
+  {
+    emailHeader: state.user.email,
+    currencyHeader: state.wallet.currency,
+    // totalHeader: state.wallet.total,
+    // expensesHeader: state.wallet.expenses.map((e) => e.exchangeRates[e.currency].ask),
+    expensesHeader: state.wallet.expenses
+      .map(({ exchangeRates, currency, value }) => +exchangeRates[currency].ask * +value),
+  }
+);
 
 export default connect(mapStateToProps)(Header);
 
 Header.propTypes = {
-  emailHeader: string,
-  currencyHeader: string,
-  totalHeader: number,
+  emailHeader: string.isRequired,
+  currencyHeader: string.isRequired,
+  expensesHeader: arrayOf(number),
+  // totalHeader: string.isRequired,
 };
 Header.defaultProps = {
-  emailHeader: '',
-  currencyHeader: 'BRL',
-  totalHeader: 0,
+  expensesHeader: [],
 };
