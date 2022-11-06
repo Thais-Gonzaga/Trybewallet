2c7b87241c94fac0c9405d039b807b246e901ca9
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-06 12:01:56 -0300

    feat: implementando a lógica na tabela seja alimentada pelo estado da aplicação
     ref: #requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,51 @@
 import React, { Component } from 'react';
+import { arrayOf, objectOf } from 'prop-types';
+import { connect } from 'react-redux';
 
 const namesHeader = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
   'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
 
 class Table extends Component {
   render() {
+    const { expensesTable } = this.props;
     return (
+
       <table>
         <thead>
           <tr>
             {namesHeader.map((name, index) => <th key={ index }>{name}</th>)}
           </tr>
         </thead>
+        <tbody>
+          {expensesTable
+            .map(({ value, description, currency, method, tag, id, exchangeRates }) => {
+              const conversion = parseFloat(+exchangeRates[currency].ask
+                * +value).toFixed(2);
+              const cambio = parseFloat(+exchangeRates[currency].ask).toFixed(2);
+              return (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{parseFloat(+value).toFixed(2)}</td>
+                  <td>{exchangeRates[currency].name}</td>
+                  <td>{cambio}</td>
+                  <td>{conversion}</td>
+                  <td>{exchangeRates.XRP.name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
       </table>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  expensesTable: state.wallet.expenses,
+});
 
-export default Table;
+export default connect(mapStateToProps)(Table);
+
+Table.propTypes = {
+  expensesTable: arrayOf(objectOf).isRequired,
+};
