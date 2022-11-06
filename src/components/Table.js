a47133f037977caba29f34a9d13460ff165ddd55
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-11-05 23:08:57 -0300

    feat: renderizando o componente Table na page Wallet
     ref: #requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+const namesHeader = ['Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
+  'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
+
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            {namesHeader.map((name, index) => <th key={ index }>{name}</th>)}
+          </tr>
+        </thead>
+      </table>
     );
   }
 }
